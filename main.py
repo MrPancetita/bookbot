5f7a62b41cef56b417e2612472ff427dd553eca4
MrPancetita <contreras.engineer@outlook.com>
2025-07-28 00:36:46 +0200

    bookbot finished according to instructions

--- main.py
+++ main.py
@@ -1,47 +1,15 @@
-def word_counter(string):
-    word_count = len(string.split())
-    return word_count  
-    
-def word_dictionary(string):
-    word_dict = {}
-    words = string.split()
-    for word in words:
-        if word in word_dict:
-            word_dict[word] += 1
-        else:
-            word_dict[word] = 1
-    return word_dict
-
-def character_dictionary(string):
-    char_dict = {}
-    for char in string:
-        char_lower = char.lower()
-        if char_lower in char_dict:
-            char_dict[char_lower] += 1
-        else:
-            char_dict[char_lower] = 1
-    return char_dict
-
-def report_generator(path_to_file, file_contents):
-    word_count = word_counter(file_contents)
-    char_dict = character_dictionary(file_contents)
-    
-    report = f"--- Begin report of {path_to_file} ---\n"
-    report += f"Word count: {word_count}\n\n"
-    report += "Character count by character:\n"
-
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
-
-    for char, count in char_dict.items():
-        if char in alphabet:
-            report += f"The character '{char}' was found {count} times\n"
-    return report
-
-
+import sys
+from stats import report_generator
 
 def main(): 
 
-    path_to_file = "./books/frankenstein.txt"
+    #path_to_file = "./books/frankenstein.txt"
+
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    path_to_file = sys.argv[1]
 
     with open(path_to_file) as f:
         file_contents = f.read() 
